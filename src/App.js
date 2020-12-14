b7916b33ce1a6653659f9efedb4f2ccecbea987a
elijasp <gitelijas@gmail.com>
2020-12-14 20:02:41 +0000

    Add SearchForm component and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from 'react';
+import SearchForm from './components/SearchForm/SearchForm';
 
 const app = () => {
   return (
     <div className="App">
-
+      <SearchForm />
     </div>
   );
 }
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './SearchForm.module.css';
+import searchIcon from './searchIcon.svg';
+
+const searchForm = () => {
+	return(
+		<form className={classes.SearchForm}>
+			<input type="search" name="search" placeholder="Search" required/>
+			<button type="submit">
+				<img src={searchIcon} alt="search icon"/>
+			</button>
+		</form>
+	);
+}
+
+export default searchForm;
