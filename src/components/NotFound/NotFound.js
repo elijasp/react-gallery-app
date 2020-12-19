bf2decbca3a9d65d9807cc1f5a8e9162cf360fdf
elijasp <gitelijas@gmail.com>
2020-12-19 22:12:04 +0000

    Add NotFound (for searches with no results) component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const notFound = () => {
+    return(
+        <div>
+            <h3>No Results Found</h3>
+            <p>Search did not return any results. Try another search.</p>
+        </div>
+    );
+}
+
+export default notFound;
