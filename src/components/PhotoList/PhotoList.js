164fea568471757028c933f2127b07b94fc66ae9
elijasp <gitelijas@gmail.com>
2020-12-19 23:02:07 +0000

    Revert "Revert "Add NotFound case scenario""
    This reverts commit d1c510d41f4b723ff6b8a158d7df906efe06df82.

--- src/components/PhotoList/PhotoList.js
+++ src/components/PhotoList/PhotoList.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Photo from '../Photo/Photo';
 import classes from './PhotoList.module.css';
+import NotFound from '../NotFound/NotFound';
 
 const photoList = (props) => {
-    const photos = props.photos.map(photo => {
-        return <Photo url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt={photo.title} key={photo.id}/>
-    });
-
+    if(props.isInitialised) {
+        const photos = props.photos.map(photo => {
+            return <Photo url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt={photo.title} key={photo.id}/>
+        });
+        
+        if(props.photos.length > 0) {
+            return(
+                <div className={classes.PhotoList}>
+                    <h2>Results</h2>
+                    <ul>{photos}</ul>
+                </div>
+            );
+        }else {
+           return <NotFound />;
+        }
+    }
     return(
-        <div className={classes.PhotoList}>
-            <h2>Results</h2>
-            <ul>{photos}</ul>
+        <div>
+            <h3>Search to view images</h3>
         </div>
-     
     );
 }
 
