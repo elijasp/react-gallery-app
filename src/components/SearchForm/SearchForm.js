735515b372856bd5f89579aa3504c17872149df3
elijasp <gitelijas@gmail.com>
2020-12-19 22:11:04 +0000

    Add search functionality callback function as props and update css module

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classes from './SearchForm.module.css';
 import searchIcon from './searchIcon.svg';
 
-const searchForm = () => {
+const searchForm = (props) => {
 	return(
-		<form className={classes.SearchForm}>
-			<input type="search" name="search" placeholder="Search" required/>
+		<form className={classes.SearchForm} onSubmit={props.onSearch}>
+			<input onChange={props.onChangeHandler} type="search" name="search" placeholder="Search" required/>
 			<button type="submit">
 				<img src={searchIcon} alt="search icon"/>
 			</button>
